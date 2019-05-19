e51a2187bae2875b4cac6c7e7fb4c0b778894784
Akira <akira.matsuda@me.com>
2019-05-20 00:36:35 +0900

    Import UIKit.

--- SDWebImageWebPCoder/Classes/UIImage+WebP.h
+++ SDWebImageWebPCoder/Classes/UIImage+WebP.h
@@ -6,6 +6,7 @@
  * file that was distributed with this source code.
  */
 
+@import UIKit;
 #import <SDWebImage/SDWebImageCompat.h>
 
 // This category is just use as a convenience method. For more detail control, use methods in `UIImage+MultiFormat.h` or directlly use `SDImageCoder`
