a62950d23f7dbd1eb21b712a9d741366ad2a94b7
DreamPiggy <lizhuoli1126@126.com>
2020-04-07 16:59:59 +0800

    Upgrade the dependency to SDWebImage 5.7, supports the maxFileSize encoding options, as well as enabling the thread-encoding for speedup

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.5.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.7.0"),
         .package(url: "https://github.com/SDWebImage/libwebp-Xcode.git", from: "1.1.0")
     ],
     targets: [
