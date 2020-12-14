19b7465deaf2241036e6b6f8111c39a58f470bc5
DreamPiggy <lizhuoli1126@126.com>
2020-12-14 11:38:10 +0800

    Update the Xcode Project of WebP encode options

--- SDWebImageWebPCoder/Classes/SDWebImageWebPCoderDefine.h
+++ SDWebImageWebPCoder/Classes/SDWebImageWebPCoderDefine.h
@@ -1,9 +1,10 @@
-//
-//  SDWebImageWebPCoderDefine.h
-//  SDWebImageWebPCoderDefine
-//
-//  Created by Antti Kortetmaa on 2020/12/06.
-//
+/*
+ * This file is part of the SDWebImage package.
+ * (c) Olivier Poitrey <rs@dailymotion.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 #if __has_include(<SDWebImage/SDWebImage.h>)
 #import <SDWebImage/SDWebImage.h>
