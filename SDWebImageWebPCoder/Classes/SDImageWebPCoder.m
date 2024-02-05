4005a4d618eb9567e7074e03213439b3eb93d12b
DreamPiggy <lizhuoli1126@126.com>
2024-02-05 22:39:13 +0800

    Copy the macros from SDWebImage Core repo
    These macro don't need compile (that SDInternalMacros.m provided the symbol), just make the compiler be able to find the search path and expand is OK

--- SDWebImageWebPCoder/Classes/SDImageWebPCoder.m
+++ SDWebImageWebPCoder/Classes/SDImageWebPCoder.m
@@ -27,48 +27,6 @@
 @import libwebp;
 #endif
 
-#define SD_USE_OS_UNFAIR_LOCK TARGET_OS_MACCATALYST ||\
-    (__IPHONE_OS_VERSION_MIN_REQUIRED >= __IPHONE_10_0) ||\
-    (__MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_12) ||\
-    (__TV_OS_VERSION_MIN_REQUIRED >= __TVOS_10_0) ||\
-    (__WATCH_OS_VERSION_MIN_REQUIRED >= __WATCHOS_3_0)
-
-#ifndef SD_LOCK_DECLARE
-#if SD_USE_OS_UNFAIR_LOCK
-#define SD_LOCK_DECLARE(lock) os_unfair_lock lock
-#else
-#define SD_LOCK_DECLARE(lock) os_unfair_lock lock API_AVAILABLE(ios(10.0), tvos(10), watchos(3), macos(10.12)); \
-OSSpinLock lock##_deprecated;
-#endif
-#endif
-
-#ifndef SD_LOCK_INIT
-#if SD_USE_OS_UNFAIR_LOCK
-#define SD_LOCK_INIT(lock) lock = OS_UNFAIR_LOCK_INIT
-#else
-#define SD_LOCK_INIT(lock) if (@available(iOS 10, tvOS 10, watchOS 3, macOS 10.12, *)) lock = OS_UNFAIR_LOCK_INIT; \
-else lock##_deprecated = OS_SPINLOCK_INIT;
-#endif
-#endif
-
-#ifndef SD_LOCK
-#if SD_USE_OS_UNFAIR_LOCK
-#define SD_LOCK(lock) os_unfair_lock_lock(&lock)
-#else
-#define SD_LOCK(lock) if (@available(iOS 10, tvOS 10, watchOS 3, macOS 10.12, *)) os_unfair_lock_lock(&lock); \
-else OSSpinLockLock(&lock##_deprecated);
-#endif
-#endif
-
-#ifndef SD_UNLOCK
-#if SD_USE_OS_UNFAIR_LOCK
-#define SD_UNLOCK(lock) os_unfair_lock_unlock(&lock)
-#else
-#define SD_UNLOCK(lock) if (@available(iOS 10, tvOS 10, watchOS 3, macOS 10.12, *)) os_unfair_lock_unlock(&lock); \
-else OSSpinLockUnlock(&lock##_deprecated);
-#endif
-#endif
-
 /// Used for animated WebP, which need a canvas for decoding (rendering), possible apply a scale transform for thumbnail decoding (avoiding post-rescale using vImage)
 /// See more in #73
 static inline CGContextRef _Nullable CreateWebPCanvas(BOOL hasAlpha, CGSize canvasSize, CGSize thumbnailSize, BOOL preserveAspectRatio) {
